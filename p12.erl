efa558596f58140c4abc69c0e7e5cab5a23cfc91
maxdnepr <maximusdn@gmail.com>
2013-01-01 22:54:51 +0200

    modified p12

--- p12.erl
+++ p12.erl
@@ -1,8 +1,19 @@
 -module(p12).
--export([decode_modified/1]).
+-export([decode_modified/1, len/1]).
 
-decode_modified([{0, List}]) ->
-   [List];
-decode_modified([{N, Element}]) ->
-   [{decode_modified(N-1, Element)}].
+decode_modified(List) ->
+   decode_modified(List, []).
+
+decode_modified([], List) ->
+   lists:reverse(p07:flatten(List));
+decode_modified([H|T], List) ->
+   decode_modified(T, [len(H)|List]).
+
+len(List) ->
+   len(List, []).
+
+len({0, _Element}, List) ->
+   List;
+len({N, Element}, List) ->
+   len({N-1, Element}, [Element|List]).
 
