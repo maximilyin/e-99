4d75a2aefcbf776bce7df81599a4dd831a059bd4
maxdnepr <maximusdn@gmail.com>
2013-01-30 00:24:17 +0200

    Update

--- create_reverse.erl
+++
@@ -1,7 +0,0 @@
--module(create_reverse).
--export([create_reverse/1]).
-
-create_reverse(1) ->
-   [1];
-create_reverse(N) ->
-   [N|create_reverse(N-1)].
--- show_number.erl
+++
@@ -1,8 +0,0 @@
--module(show_number).
--export([show_even/1]).
-
-show_even(1) ->
-   io:format("~p~n", [1]);
-show_even(N) ->
-   [io:format("~p", [N])|show_even(N-1)].
-
